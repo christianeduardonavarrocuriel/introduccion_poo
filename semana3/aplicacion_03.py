bef5810c759bd4be4da2d1f3ad66f35662bc4d8a
CHRISTIAN EDUARDO NAVARRO CURIEL <1724110319@utectulancingo.edu.mx>
2025-05-20 19:49:30 +0000

    CREATED configuracion basica 002

--- semana3/aplicacion_03.py
+++ semana3/aplicacion_03.py
@@ -19,8 +19,17 @@ class Calculadora:
         except Exception as error:
             print(f"Error 003 {error.args[0]}")
 
+    def restar (self, numero1: int, numero2: int)->int:
+        resultado = numero1 - numero2
+        return resultado
+            
 mi_calculadora = Calculadora()
 mi_calculadora.sumar(10,5)
 mi_calculadora.sumar("Hola ", "Adios")
 mi_calculadora.dividir(10.0,0.0)
 mi_calculadora.dividir("Hola",0.0)
+        
+print(f"Resultado: {mi_calculadora.restar(10,8)}")
+
+resultado = mi_calculadora.restar(8,9)
+print(f"Resultado: {resultado}")
---
+++ semana3/aplicacion_04.py
@@ -0,0 +1,25 @@
+class Mayor:
+
+    def __init__(self) -> None:
+        pass
+
+    def mayor(self,n1: int, n2:int, n3:int) -> None:
+
+        if n3 > n1 and n2:
+            print(n3)
+
+        elif n2 > n3 and n1:
+            print(n2)
+
+        else n1 >= n3
+            print(n1)
+
+mi_objeto = Mayor()
+mi_objeto.mayor(1,2,3)
+mi_objeto.mayor(1,3,2)
+mi_objeto.mayor(3,2,1)
+mi_objeto.mayor(3,1,2)
+mi_objeto.mayor(3,3,1)
+mi_objeto.mayor(3,1,3)
+mi_objeto.mayor(1,3,3)
+mi_objeto.mayor(3,3,3)
