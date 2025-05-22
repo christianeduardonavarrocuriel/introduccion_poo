e4703b7c60695c3b6850a90a38f2c062fae34234
CHRISTIAN EDUARDO NAVARRO CURIEL <1724110319@utectulancingo.edu.mx>
2025-05-22 17:35:46 +0000

    CREATED correccion sobre aplicacion_04.py

--- semana3/aplicacion_04.py
+++ semana3/aplicacion_04.py
@@ -5,14 +5,14 @@ class Mayor:
 
     def mayor(self,n1: int, n2:int, n3:int) -> None:
 
-        if n3 > n1 and n2:
-            print(n3)
+        if n1 > n2:
+            print(n1)
 
-        elif n2 > n3 and n1:
+        elif n2 > n3:
             print(n2)
 
-        else n1 >= n3
-            print(n1)
+        elif n3 >= n1:
+            print(n3)
 
 mi_objeto = Mayor()
 mi_objeto.mayor(1,2,3)
